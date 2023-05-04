635f4d4d55dfd0a502ee8184c3a331ba5a3dd0cb
Md Rejaul Karim <tuhin@10ms>
2023-05-05 05:02:39 +0600

    Initial commit

---
+++ app/src/main/java/com/rktuhinbd/splitxpens/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rktuhinbd.splitxpens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rktuhinbd.splitxpens.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
